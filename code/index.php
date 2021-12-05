e90b070bb38200974e6e3ef66834398240ae2d70
khadetou <khadetou96@gmail.com>
2021-12-05 19:41:38 +0000

    styling the index page

--- code/index.php
+++ code/index.php
@@ -5,20 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/styles.css">
     <title>Doodle</title>
 </head>
 
 <body>
-    <div class="mainSection">
-        <div class="logoContainer">
-            <img src="https://www.google.com/logos/doodles/2021/seasonal-holidays-2021-6753651837109324.3-ladc.gif" alt="Google logo">
+    <div class="wrapper indexPage">
+        <div class="mainSection">
+            <div class="logoContainer">
+                <img src="https://www.google.com/logos/doodles/2021/seasonal-holidays-2021-6753651837109324.3-ladc.gif" alt="Google logo">
+            </div>
+            <div class="searchContainer">
+                <form action="search.php" method="GET">
+                    <input type="text" class="searchBox" name="term">
+                    <input type="submit" class="searchButton" value="Search">
+                </form>
+            </div>
         </div>
-    </div>
-    <div class="searchContainer">
-        <form action="search.php" method="GET">
-            <input type="text" class="searchBox" name="term">
-            <input type="submit" class="searchButton" value="Search">
-        </form>
     </div>
 </body>
 
