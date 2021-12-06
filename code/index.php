641ea0dd3d06a45e362f2ab8ac9c53273d762d83
khadetou <khadetou96@gmail.com>
2021-12-06 11:49:50 +0000

    search box desing

--- code/index.php
+++ code/index.php
@@ -15,12 +15,43 @@
             <div class="logoContainer">
                 <img src="https://www.google.com/logos/doodles/2021/seasonal-holidays-2021-6753651837109324.3-ladc.gif" alt="Google logo">
             </div>
-            <div class="searchContainer">
-                <form action="search.php" method="GET">
-                    <input type="text" class="searchBox" name="term">
-                    <input type="submit" class="searchButton" value="Search">
-                </form>
-            </div>
+            <form class="form" action="">
+                <div>
+                    <div class="searchbox-container">
+                        <div class="searchContainer">
+                            <!-- <form action="search.php" method="GET">
+                                <input type="text" class="searchBox" name="term">
+                                <input type="submit" class="searchButton" value="Search">
+                            </form> -->
+                            <div class="subcontainer">
+                                <div class="search-icon">
+                                    <div class="searh-icon-container">
+                                        <span class="span span1 span2">
+                                            <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                                <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="input-container">
+                                    <div class="top-input"></div>
+                                    <input type="text" class="input" maxlength="2048" name="term">
+                                </div>
+                                <div class="mic-container">
+                                    <div class="mic-subcontainer">
+                                        <svg class="svg">
+                                            <path fill="#4285f4" d="m12 15c1.66 0 3-1.31 3-2.97v-7.02c0-1.66-1.34-3.01-3-3.01s-3 1.34-3 3.01v7.02c0 1.66 1.34 2.97 3 2.97z"></path>
+                                            <path fill="#34a853" d="m11 18.08h2v3.92h-2z"></path>
+                                            <path fill="#fbbc04" d="m7.05 16.87c-1.27-1.33-2.05-2.83-2.05-4.87h2c0 1.45 0.56 2.42 1.47 3.38v0.32l-1.15 1.18z"></path>
+                                            <path fill="#ea4335" d="m12 16.93a4.97 5.25 0 0 1 -3.54 -1.55l-1.41 1.49c1.26 1.34 3.02 2.13 4.95 2.13 3.87 0 6.99-2.92 6.99-7h-1.99c0 2.92-2.24 4.93-5 4.93z"></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </body>
